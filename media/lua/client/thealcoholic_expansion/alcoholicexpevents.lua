f0c6022e310cd02472eace4bbd25a8b1f304f1f8
Nick Slusarczyk <axxessdenied@gmail.com>
2024-08-22 18:41:20 -0400

    Basic implementation

--- media/lua/client/thealcoholic_expansion/alcoholicexpevents.lua
+++ media/lua/client/thealcoholic_expansion/alcoholicexpevents.lua
@@ -2,12 +2,15 @@
 
 local function addAlcoholicItems(playernum, character)
     local player = getSpecificPlayer(playernum)
-    local x = ZombRandFloat(0.1, 0.9)
-    local y = ZombRandFloat(0.1, 0.9)
-    local z = player:getZ() - math.floor(player:getZ())
-    player:getInventory():AddItem("TheAlcoholic.HoochFull")
-    --player:getCurrentSquare():AddWorldInventoryItem("TheAlcoholic.HoochFull", x, y, z)
-    --triggerEvent("OnContainerUpdate")
+    if player:HasTrait("Alcoholic") and ZombRand(3) == 0
+    then
+        local x = ZombRandFloat(0.1, 0.9)
+        local y = ZombRandFloat(0.1, 0.9)
+        local z = player:getZ() - math.floor(player:getZ())
+    
+        player:getCurrentSquare():AddWorldInventoryItem("AlcoholicItems.HoochFull", x, y, z)
+    end
+    triggerEvent("OnContainerUpdate")
 end
 
---Events.OnCreatePlayer.Add(addAlcoholicItems)
+Events.OnCreatePlayer.Add(addAlcoholicItems)
